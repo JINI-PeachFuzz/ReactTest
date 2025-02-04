f98ec88eec69b771ba962f46c8f274f8ac135542
JINJU PARK <aricclunj@naver.com>
2025-02-04 09:57:24 +0900

    .

---
+++ exam02/src/app/member/containers/LoginContainer.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useState, useActionState } from 'react'
+import { processLogin } from '../services/actions'
+import LoginForm from '../components/LoginForm'
+const LoginContainer = () => {
+  const [form, setForm] = useState<{ email?: string; password?: string }>({})
+  const actionState = useActionState(processLogin, form)
+
+  const handleChange = (e) =>
+    setForm((form) => ({ ...form, [e.target.name]: e.target.value }))
+
+  return <LoginForm form={form} onChange={handleChange} />
+}
+
+export default LoginContainer
